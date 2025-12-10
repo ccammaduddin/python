89ec28a44f806f304ba982ab974e2007f9c00855
AMMAD DIN <AMMADDIN2@BAGH.MTBC.COM>
2025-12-10 16:39:00 +0500

    changes made

---
+++ 15_for_loops_and_nesting.py
@@ -0,0 +1,6 @@
+for i in range(0,20,2):
+    print(i)
+
+friends_names = ["Ammad", "Najaf", "Hasnain", "Sahad", "Mudassir"]
+for index in range (len(friends_names)):
+    print(friends_names[index])
