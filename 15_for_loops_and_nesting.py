bd92ac49207f99c0c7b1f4883df9b04f182a9a5d
AMMAD DIN <AMMADDIN2@BAGH.MTBC.COM>
2025-12-30 09:07:52 +0500

    changes made

--- 15_for_loops_and_nesting.py
+++ 15_for_loops_and_nesting.py
@@ -1,19 +1,16 @@
-def separator():
-    print("..............................................")
-
 for i in range(2,21,2): # start, end, step
     print(i)
 
-separator()
+print("..............................................")
 
 friends_names = ["Ammad", "Najaf", "Hasnain", "Sahad", "Mudassir"]
 for index in range (len(friends_names)): # get the length of array
-    if friends_names[index] == "Hasnain": #chek if at the index friend name is hasnain
+    if friends_names[index] == "Hasnain": #check if at the index friend name is hasnain
         print("Found " + friends_names[index] + "!") 
         break # break the loop
     print(friends_names[index]) # gets the friend name at the index, friend_names[1] will get the second element of the array
 
-separator()
+print("..............................................")
 
 for name in friends_names:
     if name == "Sahad":
@@ -21,7 +18,7 @@ for name in friends_names:
         continue # skip the current iteration
     print(name)
 
-separator()
+print("..............................................")
 
 for friend in friends_names:
     for number in range(1, 4):
